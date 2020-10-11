7304a8ccf6a5649a0910680e025ffdab10b09504
Mona Javed Syed <monajaved93@gmail.com>
2020-10-11 16:57:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,3 +10,11 @@ def display_board(board)
   puts "   |   |   "
 end
 display_board(board)
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+# puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
+# puts "-----------"
+# puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
+# puts "-----------"
+# puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
+display_board(board)
