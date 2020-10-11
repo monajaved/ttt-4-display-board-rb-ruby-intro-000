ae0188b6eebc97808024a81c387b149f93da6cb9
Mona Javed Syed <monajaved93@gmail.com>
2020-10-11 17:02:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,18 +3,19 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts "   |   |   "
+  # puts "   |   |   "
+  # puts "-----------"
+  # puts "   |   |   "
+  # puts "-----------"
+  # puts "   |   |   "
+  puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
 display_board(board)
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
-# puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
-# puts "-----------"
-# puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
-# puts "-----------"
-# puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
+# board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+# display_board(board)
+
